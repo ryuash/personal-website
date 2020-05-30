68af617f5da4b1a5891e8f0893970ddb0ff610b5
Wingman Lee <wingman@appit.com.hk>
2020-05-30 22:13:28 +0800

    Fix: ssr window screen listener

--- src/utils/screenSize.js
+++ src/utils/screenSize.js
@@ -1,24 +1,31 @@
 import { useState, useEffect } from 'react';
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
+function useWindowSize() {
+  const isClient = typeof window === 'object';
 
-export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  function getSize() {
+    return {
+      width: isClient ? window.innerWidth : undefined,
+      height: isClient ? window.innerHeight : undefined,
+    };
+  }
+
+  const [windowSize, setWindowSize] = useState(getSize);
 
   useEffect(() => {
+    if (!isClient) {
+      return false;
+    }
+
     function handleResize() {
-      setWindowDimensions(getWindowDimensions());
+      setWindowSize(getSize());
     }
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return windowDimensions;
+  return windowSize;
 }
+
+export default useWindowSize;
